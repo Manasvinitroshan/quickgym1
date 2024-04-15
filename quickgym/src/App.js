ffbc3ab38340c671765b9724c1382445179ad1d5
Manasvinitroshan <manas.comet.25@gmail.com>
2024-04-14 19:12:17 -0500

    First commit

---
+++ quickgym/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+      hi this is manas and i have a really cool web dev team
+      
+    </div>
+  );
+}
+
+export default App;
